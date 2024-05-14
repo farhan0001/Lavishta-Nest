c4d1d56e09b7db49f372ae54bcf77ed4ffcce27a
farhan0001 <farqua0@gmail.com>
2024-05-14 07:12:57 +0530

    Modified to run on single Port

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-// const path = require('path');
+const path = require('path');
 
 if(process.env.NODE_ENV !== 'PRODUCTION'){
     require('dotenv').config({path: "backend/config/config.env"});
@@ -27,11 +27,11 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-// app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+})
 
 app.use(errorMiddleware);
 
